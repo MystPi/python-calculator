b3822082670ee50cff689b61b460ac03d05ce78c
MystPi <86574651+MystPi@users.noreply.github.com>
2021-09-24 14:23:48 -0400

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,42 +1,43 @@
 operators = {
-	'+': (0, 'left'),
-	'-': (0, 'left'),
-	'*': (1, 'left'),
-	'/': (1, 'left'),
-	'%': (1, 'left'),
-	'^': (2, 'right')
+    '+': (0, 'left'),
+    '-': (0, 'left'),
+    '*': (1, 'left'),
+    '/': (1, 'left'),
+    '%': (1, 'left'),
+    '^': (2, 'right')
 }
 
 
 def tokenize(text: str) -> list:
-	ret = []
-	current = ''
+    text = text.replace(' ', '')
+    ret = []
+    current = ''
+    i = 0
+    
+    for char in text:
+        if char in operators:
+            if char == '-' and (i == 0 or (text[i + 1].isdigit() and text[i - 1] in operators)):
+                current += char
+                i += 1
+                continue
+            if len(current) > 0:
+                ret.append(current)
+            ret.append(char)
+            current = ''
+        elif char in '()':
+            if len(current) > 0:
+                ret.append(current)
+            ret.append(char)
+            current = ''
+        elif char == ' ':
+            if len(current) > 0:
+                ret.append(current)
+            current = ''
+        else:
+            current += char
+        i += 1
 
-	i = 0
-	for char in text:
-		if char in operators:
-			if char == '-' and (i == 0 or (text[i + 1].isdigit())):
-				current += char
-				continue
-			if len(current) > 0:
-				ret.append(current)
-			ret.append(char)
-			current = ''
-		elif char in '()':
-			if len(current) > 0:
-				ret.append(current)
-			ret.append(char)
-			current = ''
-		elif char == ' ':
-			if len(current) > 0:
-				ret.append(current)
-			current = ''
-		else:
-			current += char
-		i += 1
+    if len(current) > 0:
+        ret.append(current)
 
-	if len(current) > 0:
-		ret.append(current)
-
-	return ret
-
+    return ret
